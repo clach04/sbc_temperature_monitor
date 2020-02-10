53d9804d74ea1f14f0727eb144592b9600265b25
clach04 <clach04@gmail.com>
2020-02-09 16:58:03 -0800

    Start to make plotting more module like

--- stress_plot.py
+++ stress_plot.py
@@ -6,28 +6,44 @@
 
 import glob
 import json
+import sys
 
 import pygal
 
 
-t_chart =  pygal.XY(stroke=True)
-t_chart.title = 'Temperatures'
-t_chart.x_title = 'Time (seconds)'
-t_chart.y_title = 'Temperature (C)'
-t_chart.truncate_legend = 30  # or -1 to disable
+def generate_pygal_chart():
+    t_chart =  pygal.XY(stroke=True)
+    t_chart.title = 'Temperatures'
+    t_chart.x_title = 'Time (seconds)'
+    t_chart.y_title = 'Temperature (C)'
+    t_chart.truncate_legend = 30  # or -1 to disable
 
-#TODO Move legend to right? not sure this is possible, move to bottom is
+    #TODO Move legend to right? not sure this is possible, move to bottom is
 
-for filename in glob.glob('*.json'):
-    #print(filename)
-    file_in = open(filename)
-    data = json.load(file_in)
-    file_in.close()
+    for filename in glob.glob('*.json'):
+        #print(filename)
+        file_in = open(filename)
+        data = json.load(file_in)
+        file_in.close()
 
-    t_chart.add(data['name'], zip(data['time'], data['temperature']))
+        t_chart.add(data['name'], zip(data['time'], data['temperature']))
+    return t_chart
 
 
-#result = t_chart.render()
-#print(result)  # dump SVG to stdout - py2 only
-t_chart.render_to_file('chart.svg')  # py2 and py3
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+    t_chart = generate_pygal_chart()
+    #result = t_chart.render()
+    #print(result)  # dump SVG to stdout - py2 only
+    t_chart.render_to_file('chart.svg')  # py2 and py3
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
+
 
